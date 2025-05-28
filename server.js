c989942f8298ae59657e37d73bedfa0800cb3ef3
World-of-treants <91582194+World-of-treants@users.noreply.github.com>
2025-05-28 14:44:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -50,4 +50,8 @@ app.get("/roll-first", (req, res) => {
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log("Listening on port", PORT);
+});
+
